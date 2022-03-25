de75238ab8e926efc9fd78e012620d333b491c8e
Stathis Stathopoulos <stathopoulos.stathis98@gmail.com>
2022-03-26 01:05:22 +0200

    Sample code of a triangle draw

--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -47,8 +47,30 @@ int main(int args, char** argv) {
 	// Set clear color
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-	// Creating shader
+	// Shader
 	Shader shader("shader.vert", "shader.frag");
+
+	// Creating a triangle - Sample Code
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// Create a vertex array object
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Create a vertex buffer object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Set attribute pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
@@ -57,6 +79,9 @@ int main(int args, char** argv) {
 
 		// Clear color buffer
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
