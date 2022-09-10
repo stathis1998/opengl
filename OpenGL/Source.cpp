731efde1bb9795d3fb8f022b1f31ca3d171161a0
Stathis Stathopoulos <stathopoulos.stathis98@gmail.com>
2022-09-10 21:53:16 +0300

    Re-write basic window code

--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -3,160 +3,69 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include "Shader.h"
+void proccessInput(GLFWwindow* window);
 
 int main(int args, char** argv) {
-	// Initialize GLFW
+	// Initializing glfw
 	if (!glfwInit()) {
-		std::cerr << "ERROR::GLFW::INIT_FAILED" << std::endl;
+		std::cerr << "Failed to initialize glfw" << std::endl;
 		return -1;
 	}
 
-	// Set OpenGL version
+	// Adding window hints
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	// Create window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+	// Creating window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
 	if (!window) {
-		std::cerr << "ERROR::GLFW::WINDOW_FAILED" << std::endl;
+		std::cerr << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	// Set context
+	// Setting window as current context
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
+	// Initializing glew
 	if (glewInit() != GLEW_OK) {
-		std::cerr << "ERROR::GLEW::INIT_FAILED" << std::endl;
+		std::cerr << "Failed to initialize glew" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
 
-	// Set glew experimental
-	glewExperimental = GL_TRUE;
-
-	// Set viewport
+	// Setting viewport
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
-	// Set clear color
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	// Updating viewport
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		glViewport(0, 0, width, height);
+	});
 
-	// Shader
-	Shader shader("shader.vert", "shader.frag");
-
-	// Creating a square using indices
-	float vertices[] = {
-				-0.5f, -0.5f, -0.5f,
-				 0.5f, -0.5f, -0.5f,
-				 0.5f,  0.5f, -0.5f,
-				 0.5f,  0.5f, -0.5f,
-				-0.5f,  0.5f, -0.5f,
-				-0.5f, -0.5f, -0.5f,
-
-				-0.5f, -0.5f,  0.5f,
-				 0.5f, -0.5f,  0.5f,
-				 0.5f,  0.5f,  0.5f,
-				 0.5f,  0.5f,  0.5f,
-				-0.5f,  0.5f,  0.5f,
-				-0.5f, -0.5f,  0.5f,
-
-				-0.5f,  0.5f,  0.5f,
-				-0.5f,  0.5f, -0.5f,
-				-0.5f, -0.5f, -0.5f,
-				-0.5f, -0.5f, -0.5f,
-				-0.5f, -0.5f,  0.5f,
-				-0.5f,  0.5f,  0.5f,
-
-				 0.5f,  0.5f,  0.5f,
-				 0.5f,  0.5f, -0.5f,
-				 0.5f, -0.5f, -0.5f,
-				 0.5f, -0.5f, -0.5f,
-				 0.5f, -0.5f,  0.5f,
-				 0.5f,  0.5f,  0.5f,
-
-				-0.5f, -0.5f, -0.5f,
-				 0.5f, -0.5f, -0.5f,
-				 0.5f, -0.5f,  0.5f,
-				 0.5f, -0.5f,  0.5f,
-				-0.5f, -0.5f,  0.5f,
-				-0.5f, -0.5f, -0.5f,
-
-				-0.5f,  0.5f, -0.5f,
-				 0.5f,  0.5f, -0.5f,
-				 0.5f,  0.5f,  0.5f,
-				 0.5f,  0.5f,  0.5f,
-				-0.5f,  0.5f,  0.5f,
-				-0.5f,  0.5f, -0.5f,
-	};
-
-	// Create a vertex array object
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	// Create a vertex buffer object
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Set attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(0);
-
-	// Enable depth test
-	glEnable(GL_DEPTH_TEST);
-
-	// Create projection matrix
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 0.1f, 100.0f);
-
-	// Create view matrix
-	glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
-	// Set shader uniforms
-	shader.setMat4("projection", projection);
-	shader.setMat4("view", view);
-
-	// Box locations
-	glm::vec3 boxes[] = {
-		{ -5.0f, 1.0f, 1.0f },
-		{ -3.0f, 1.0f, 1.0f },
-		{ -1.0f, 1.0f, 1.0f },
-		{ 1.0f, 1.0f, 1.0f },
-		{ 3.0f, 1.0f, 1.0f },
-		{ 5.0f, 1.0f, 1.0f },
-	};
-
-	// Main loop
+	// Rendering loop
 	while (!glfwWindowShouldClose(window)) {
-		// Poll events
+		// Checking for events
 		glfwPollEvents();
 
-		// Clear color buffer & depth buffer
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Processing input
+		proccessInput(window);
 
-		for (int i = 0; i < 6; i++) {
-			// Make the cube spin slowly
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, glm::vec3(boxes[i][0], boxes[i][1], boxes[i][2]));
-			model = glm::rotate(model, glm::radians((float) glfwGetTime() * 30 * (i + 1)), glm::vec3(0.0f, 1.0f, 1.0f));
-			shader.setMat4("model", model);
-			// Draw square
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-		// Swap buffers
+		// Rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swapping buffers
 		glfwSwapBuffers(window);
 	}
 
 	return 0;
+}
+
+void proccessInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
